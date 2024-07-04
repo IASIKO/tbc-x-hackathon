d5d12746fbcd36a84650ab4dc8920a8f61397b9d
IASIKO <iaseshviligi@gmail.com>
2024-07-04 13:13:57 +0400

    added marketplace content

--- app/dashboard/(dashboard)/marketplace/page.tsx
+++ app/dashboard/(dashboard)/marketplace/page.tsx
@@ -1,7 +1,5 @@
+import MarketplaceComp from "@/components/dashboard/Marketplace/MarketplaceComp";
+
 export default function Marketplace() {
-  return (
-    <main className="p-16">
-      <h1 className="text-[40px] font-bold">Marketplace Page</h1>
-    </main>
-  );
+  return <MarketplaceComp />;
 }
--- components/dashboard/Layout/SideBar.tsx
+++ components/dashboard/Layout/SideBar.tsx
@@ -14,16 +14,18 @@ const SideBar = () => {
   const pathName = usePathname();
 
   return (
-    <div className="fixed z-50 xl:w-[300px] w-auto border-r-2 border-r-[#AFAFAF] h-full">
+    <div className="fixed z-50 xl:w-[300px] w-auto border-r-2  h-full">
       <div className="p-4 flex flex-col items-center text-center">
-        <h1 className="text-[40px] font-bold text-[#1CB0F6] hidden xl:block">6th Element</h1>
+        <h1 className="text-[40px] font-bold text-[#1CB0F6] hidden xl:block">
+          6th Element
+        </h1>
         <div className="pt-6 flex flex-col gap-6">
           <Link
             href="/dashboard/learn"
             className={`${
               pathName === "/dashboard/learn"
-                ? "text-[22px] font-bold flex items-center gap-4 border border-[#1CB0F6] p-2 px-4 rounded-xl  text-[#1CB0F6] bg-[#DDF4FF] w-auto xl:w-[240px]"
-                : "text-[22px] font-bold flex items-center gap-4 text-[#777777]"
+                ? "text-[22px] font-bold flex justify-center xl:justify-start items-center gap-4 border border-[#1CB0F6] p-2 px-4 rounded-xl  text-[#1CB0F6] bg-[#DDF4FF] w-auto xl:w-[240px]"
+                : "text-[22px] font-bold flex justify-center xl:justify-start items-center gap-4 text-[#777777]"
             } `}
           >
             <Image src={learnIcon} alt="Learn icon" className="w-12 h-12" />{" "}
@@ -33,8 +35,8 @@ const SideBar = () => {
             href="/dashboard/simulate"
             className={`${
               pathName === "/dashboard/simulate"
-                ? "text-[22px] font-bold flex items-center gap-4 border border-[#1CB0F6] p-2 px-4 rounded-xl  text-[#1CB0F6] bg-[#DDF4FF] w-auto xl:w-[240px]"
-                : "text-[22px] font-bold flex items-center gap-4 text-[#777777]"
+                ? "text-[22px] font-bold flex justify-center xl:justify-start items-center gap-4 border border-[#1CB0F6] p-2 px-4 rounded-xl  text-[#1CB0F6] bg-[#DDF4FF] w-auto xl:w-[240px]"
+                : "text-[22px] font-bold flex justify-center xl:justify-start items-center gap-4 text-[#777777]"
             } `}
           >
             <Image
@@ -48,8 +50,8 @@ const SideBar = () => {
             href="/dashboard/marketplace"
             className={`${
               pathName === "/dashboard/marketplace"
-                ? "text-[22px] font-bold flex items-center gap-4 border border-[#1CB0F6] p-2 px-4 rounded-xl  text-[#1CB0F6] bg-[#DDF4FF] w-auto xl:w-[240px]"
-                : "text-[22px] font-bold flex items-center gap-4 text-[#777777]"
+                ? "text-[22px] font-bold flex justify-center xl:justify-start items-center gap-4 border border-[#1CB0F6] p-2 px-4 rounded-xl  text-[#1CB0F6] bg-[#DDF4FF] w-auto xl:w-[240px]"
+                : "text-[22px] font-bold flex justify-center xl:justify-start items-center gap-4 text-[#777777]"
             } `}
           >
             <Image
@@ -61,12 +63,12 @@ const SideBar = () => {
           </Link>
           <div
             onClick={() => setOtherModal(!otherModal)}
-            className="text-[22px] font-bold text-[#777777] flex items-center gap-4 relative cursor-pointer"
+            className="text-[22px] font-bold text-[#777777] flex justify-center xl:justify-start items-center gap-4 relative cursor-pointer"
           >
             <Image src={othersIcon} alt="Others icon" className="w-12 h-12" />{" "}
             <span className="hidden xl:block">Others</span>
             {otherModal && (
-              <div className="absolute top-16 w-[240px] flex flex-col gap-4 border border-[#4B4B4B] bg-[#DDF4FF] rounded p-4">
+              <div className="absolute top-16 left-2 xl:left-0 w-[240px] flex flex-col gap-4 border border-[#1CB0F6] bg-[#DDF4FF] rounded p-4">
                 <Link
                   href="/dashboard/profile"
                   className="bg-[#1CB0F6] rounded-lg p-2 text-black"
